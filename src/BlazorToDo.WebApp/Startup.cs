90cea0a57882a351ca07a8b490cccb5d29c45299
Olivier Beaudoin <contact@olivierbeaudoin.ca>
2019-09-15 11:55:17 -0400

    Upgrade to Preview9 Removed Identity, EFCore

--- src/BlazorToDo.WebApp/Startup.cs
+++ src/BlazorToDo.WebApp/Startup.cs
@@ -4,15 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorToDo.WebApp.Areas.Identity;
 using BlazorToDo.WebApp.Data;
 using BlazorToDo.WebApp.Services;
 
@@ -30,15 +26,9 @@ namespace BlazorToDo.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+        {            
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddScoped<AuthenticationStateProvider, RevalidatingAuthenticationStateProvider<IdentityUser>>();
 
             services.AddSingleton<IToDoUpdateService, ToDoUpdateService>();
             services.AddHostedService<ScheduledTaskHostedService>();
@@ -50,7 +40,6 @@ namespace BlazorToDo.WebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -63,9 +52,6 @@ namespace BlazorToDo.WebApp
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
